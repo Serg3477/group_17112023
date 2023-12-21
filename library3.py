f66cb8f9dcc7478090ebbb3197bcf235553ce8fd
Sergey Budaev <sergeybudaev999@gmail.com>
2023-12-21 11:51:44 +0200

    Working with functions. Part 2.

--- library3.py
+++ library3.py
@@ -1,6 +1,5 @@
-def working_with_dict_from_input_walues(my_dict: dict) -> str:
-    distance = my_dict['time='] * my_dict['speed=']
-    result = ("Транспортний засіб вагою " + str(my_dict['weight=']) + " кг рухався "
-              + str(my_dict['time=']) + " секунд зі швидкістю " + str(my_dict['speed='])
-              + " м/сек, і подолав відстань " + str(distance) +" метрів.")
+def working_with_dict_from_input_values(time_input: str, speed_input: str, weight_input: str) -> str:
+    distance = int(time_input) * int(speed_input)
+    result = (f'Транспортний засіб вагою {weight_input} кг рухався {time_input} секунд зі швидкістю {speed_input}'
+              f' м/сек, і подолав відстань {distance} метрів.')
     return result
--- test_library3.py
+++ test_library3.py
@@ -1,23 +1,15 @@
+import lesson12
 import library3
 import pytest
 
 
-def working_with_dict_from_input_walues(my_dict: dict) -> str:
-    if my_dict['time='] < 0:
-        raise ValueError ('Введ іть корректне значення.')
-    if my_dict['speed='] < 0:
-        raise ValueError ('Введ іть корректне значення.')
-    if my_dict['weight='] < 0:
-        raise ValueError ('Введ іть корректне значення.')
-    distance = my_dict['time='] * my_dict['speed=']
-    result = ("Транспортний засіб вагою " + str(my_dict['weight=']) + " кг рухався " + str(my_dict['time='])
-              + " секунд зі швидкістю " + str(my_dict['speed=']) + " м/сек, і подолав відстань " + str(distance) +" метрів.")
-    
+def test_working_with_dict_from_input_values():
     my_time = '20'
     my_speed = '10'
     my_weight = '1000'
-    distance = my_time * my_speed
-    expected = ("Транспортний засіб вагою " + str(my_weight) + " кг рухався " + str(my_time) + " секунд зі швидкістю "
-                + str(my_speed) + " м/сек, і подолав відстань " + str(distance) +" метрів.")
-    assert result == expected
+    expected_distance = '300'
+    distance = int(my_time) * int(my_speed)
+    actual_result = library3.working_with_dict_from_input_values(my_time, my_speed, my_weight)
+    expected = (f'Транспортний засіб вагою {my_weight} кг рухався {my_time} секунд зі швидкістю {my_speed} м/сек, і подолав відстань {distance} метрів.')
+    assert actual_result == expected
     
