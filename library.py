e1afdbed384668ff3e8fb5ac66a488bb4d3e2a81
Sergey Budaev <sergeybudaev999@gmail.com>
2023-12-21 10:41:33 +0200

    Working with functions. Part 2.

--- library.py
+++ library.py
@@ -3,10 +3,9 @@ import constants
 
 
 def convert_santimetres_to_inches (santimeter: int | float) -> float:
-    while True:
-        converted_float_value = float(santimeter)
-        converted_inch_value = round(converted_float_value/constants.INCH, 2)
-        return converted_inch_value
+    converted_float_value = float(santimeter)
+    converted_inch_value = round(converted_float_value/constants.INCH, 2)
+    return converted_inch_value
 
 
 def get_paired_numbers(numbers: list[int]) -> list[int]:
@@ -21,9 +20,10 @@ def get_paired_numbers(numbers: list[int]) -> list[int]:
 def get_ipoteka_result(salary_input: int, ipoteka_sum: int) -> bool:
     ipoteka_years = 25
     month_in_year = 12
+    percent_of_month_income = 35
+    salary_input_in_percents = int(salary_input)/100 * percent_of_month_income
     ipoteka_month = int(ipoteka_sum)/ ipoteka_years / month_in_year
-    salary_input_percent = int(salary_input)/ 100 * 35
-    result: bool = salary_input_percent > ipoteka_month
+    result: bool = salary_input_in_percents > ipoteka_month
     return result
 
 
