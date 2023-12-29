9d082424d2f089836d62e93692cf2ead9f3a5775
Sergey_Budaev <sergeybudaev999@gmail.com>
2023-12-29 18:52:09 +0200

    Working with decorators

--- lesson13.py
+++ lesson13.py
@@ -1,21 +1,16 @@
 def create_csv_to_write_logs(func: callable):
-    def inner(dict_time, dict_result):
-        result = func(dict_time, dict_result)
-        print(result)
+    def inner(*args):
+        dict_time = input("Введіть час: ")
+        dict_result = input("Введіть результат: ")
+        with open('file_logs.csv', mode='a', encoding='utf-8') as file:
+            file.write(f'{dict_time}; {dict_result}\n')
+        with open('file_logs.csv', mode='r', encoding='utf-8') as file:
+            result_reading_file = file.read()
+        return func(*args)
     return inner
 
-
-def input_logs(dict_time: str, dict_result: str):
-    with open('file_logs.csv', mode='a', encoding='utf-8') as file:
-        file.write(f'{dict_time}; {dict_result}\n')
-    with open('file_logs.csv', mode='r', encoding='utf-8') as file1:
-        result_reading_file = file1.read()
-    return result_reading_file
-
+@create_csv_to_write_logs
+def input_logs():
+    print('В файл "file_logs.csv" записано успішно.')
     
-input_logs = create_csv_to_write_logs(input_logs)
-dict_time = input("Введіть час: ")
-dict_result = input("Введіть результат: ")
-input_logs(dict_time, dict_result)
-   
-     
+input_logs()
